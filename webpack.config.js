17ae236357d93bf879d2fd98880fd23d35755f2a
Lemuel Bulangis <lemgrb@gmail.com>
2021-11-06 09:43:56 +0800

    Environment setup (#1)
    * Configure webpack, babel, css loader
    
    * Setup tailwindcss

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test:/\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test:/\.css$/,
+        use: ['style-loader',
+						{loader:'css-loader', options:{importLoaders:1}},
+						'postcss-loader']
+      }
+    ]
+  },
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000
+  }
+};
