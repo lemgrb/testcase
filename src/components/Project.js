be46e7bb4d3f27f0ddcc858089ca8124696abec8
Lemuel Bulangis <lemgrb@gmail.com>
2021-11-07 08:33:34 +0800

    [Fix] Project name not wrapped when words have no spaces

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Project = (props) => {
     return (
         <div className="border p-1 m-2  hover:bg-yellow-100">
-            <h1 className="text-lg font-semibold underline text-yellow-600"><a href="#">{props.name}</a></h1>
+            <h1 className="text-lg font-semibold underline text-yellow-600 break-all"><a href="#">{props.name}</a></h1>
             <p className="text-sm">{props.description}</p>
         </div>
     )
