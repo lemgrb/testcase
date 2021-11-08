14d0a06aa4a73fc9404f212ed4301d5a26b46ade
Lemuel Bulangis <lemgrb@gmail.com>
2021-11-08 15:52:17 +0800

    Add cypress tests

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const Project = ({name, description, ...props}) => {
     return (
-        <div className="border p-1 m-2  hover:bg-yellow-100">
+        <div className="project-list-item border p-1 m-2  hover:bg-yellow-100">
             <h1 className="text-lg font-semibold underline text-yellow-600 break-all"><a href="#">{name}</a></h1>
             <p className="text-sm">{description}</p>
         </div>
